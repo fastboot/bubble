daf8f80c084e88a895c35ba0da46f8f44a3785ac
fastboot <pranjal2k16@gmail.com>
2021-03-23 02:22:09 +0530

    Trying spotify api and it works

--- src/components/Spotify/NowPlaying/index.js
+++ src/components/Spotify/NowPlaying/index.js
@@ -1,26 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
-const getData = async (name) => {
+// const getData = async (name) => {
 
-    const url = 'https://api.spotify.com/v1/me/player?market=IN&additional_types=episode'
-    const response = await axios.get(url, {
-        headers: {
-            'Authorization': 'Bearer BQAztao7yQDm3Z0DN-fH4rmXeB5oNKWKyzRYLLfZALO7N8bd1ztmxanZr2dANCPBy6z8u7quJQwcpvIXrPvhVYcx1_U7lSwLLLpjyJdat89f_SR4zYg0R54EVYqjVBzzywIzq0AYGTVsWTkmapYzdRhwr8PxXWK5764JHg9c5mvRXwplOz5R9e8',
-        }
-    })
-    console.log(response.data)
-    // return (response.data)
-    // return {status:400, message: JSON.stringify(response.data)};
-}
+//     const url = 'https://api.spotify.com/v1/me/player?market=IN&additional_types=episode'
+//     const response = await axios.get(url, {
+//         headers: {
+//             'Authorization': 'Bearer BQBFNBEpohZTMErHumstckpZDPVs_731pJPjZeTYO4Kk3MV_7KwoPstQxaHAOq7O3N9rkpzXY7Zx6hgnlOJYSYDdWjeL7LV8LDw9j-97ccD0wFjbtqsjSJTsbhpr12G495m2vOtwMir_CzwNx-Br5S6LwokScGrXFIMhamIJFn-IQ3fTF6EmniY',
+//         }
+//     })
+//     console.log(response.data)
+//     return (response.data)
+//     // return {status:400, message: JSON.stringify(response.data)};
+// }
 
+// useEffect(() => {     
+//     const getData = async () => {  
+//       await axios.get('your_url')  
+//       .then(res => {  
+//         console.log(res)  
+//       })  
+//       .catch(err => {  
+//         console.log(err)  
+//       });  
+//     }  
+//     getData()  
+//   }, [])
 
 function NowPlaying() {
-    getData();
+    
+    // getData();
+    const [track, setTrack] = useState('');
+    useEffect(() => {
+        const getData = async () => {
+        const url = 'https://api.spotify.com/v1/me/player?market=IN&additional_types=episode'
+        const response = await axios.get(url, {
+            headers: {
+                'Authorization': 'Bearer BQBFNBEpohZTMErHumstckpZDPVs_731pJPjZeTYO4Kk3MV_7KwoPstQxaHAOq7O3N9rkpzXY7Zx6hgnlOJYSYDdWjeL7LV8LDw9j-97ccD0wFjbtqsjSJTsbhpr12G495m2vOtwMir_CzwNx-Br5S6LwokScGrXFIMhamIJFn-IQ3fTF6EmniY',
+            }
+        })
+        console.log(response.data)
+        setTrack(response.data.item.name)
+    };
+        getData();
+    },[])
     // const song = getData().then((res) => res.data);
+
     return (
         <div>
-            <h1> Now Playing </h1>
+            <h1> {track} </h1>
         </div>
     )
 }
