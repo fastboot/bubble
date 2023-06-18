96a40b2fb1b1678d32ccf7ce45cb0d5ef2d27494
fastboot <pranjal2k16@gmail.com>
2023-06-18 14:27:26 +0530

    refactor: change linkedin link

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,5 +2,5 @@
 export const FACEBOOK = 'https://www.facebook.com/shanecastIino/'
 export const INSTAGRAM = 'https://www.instagram.com/pranjalmishra.11/'
 export const TWITTER = 'https://twitter.com/shanecastlino'
-export const LINKEDIN = 'https://www.linkedin.com/in/fastboot/'
+export const LINKEDIN = 'https://www.linkedin.com/in/pranjalmishra11/'
 export const GITHUB = 'https://www.github.com/fastboot'
