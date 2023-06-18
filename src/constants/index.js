bf03524af18d13dacbf222e638bb4ff2b3ae59f5
fastboot <pranjal2k16@gmail.com>
2023-06-18 14:19:35 +0530

    refactor: change insta link

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 // social links
 export const FACEBOOK = 'https://www.facebook.com/shanecastIino/'
-export const INSTAGRAM = 'https://www.instagram.com/shanecastlino/'
+export const INSTAGRAM = 'https://www.instagram.com/pranjalmishra.11/'
 export const TWITTER = 'https://twitter.com/shanecastlino'
 export const LINKEDIN = 'https://www.linkedin.com/in/fastboot/'
 export const GITHUB = 'https://www.github.com/fastboot'
